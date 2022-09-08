425919d046dee716eda22f459616f422b6d29922
THIAGO-BARBOSA-ARAUJO <thiagobarbosa.araujo@outlook.com>
2022-09-07 23:16:59 -0300

    tela de login desingner feito

---
+++ src/components/login/Login.js
@@ -0,0 +1,195 @@
+import { useNavigate } from "react-router-dom"
+import { useState } from "react"
+import styled from "styled-components"
+import axios from "axios"
+
+export default function Login(){
+    
+    const [email, setEmail] = useState("")
+    const [senha, setSenha] = useState("")
+
+    let navigate = useNavigate()
+
+    function fazerLogin(event) {
+		event.preventDefault();
+
+		try{
+			const requisicao = axios.post("", {
+            email: email,
+            password: senha
+		})
+
+            requisicao.then((req)=>{
+                //localStorage.setItem("imguser", req.data.image)
+                //localStorage.setItem("token", req.data.token)
+                navigate("/habitos")
+            })
+            .catch(()=>{
+                console.log("deu ruim na req parça")
+            })
+
+		}catch {
+			console.log("deu ruim")
+            
+            setEmail("")
+            setSenha("")
+		}
+	}
+
+    return(
+        <div className="login">
+            <Styledlogin>
+            <form onSubmit={fazerLogin}>
+                
+                <h1>MyWallet</h1>
+                <input required type="email" id="campoemail" placeholder="email" value={email} onChange={e => setEmail(e.target.value)} /><br/>
+                
+                <input required type="password" id="camposenha" placeholder="senha" value={senha} onChange={e => setSenha(e.target.value)} /><br/>
+                <button>Entrar</button>
+                <p onClick={()=>{navigate("")}}>Não tem uma conta? Cadastre-se!</p>
+            </form>
+        </Styledlogin>
+            
+        </div>
+
+    )
+}
+
+
+const Styledlogin = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background: #8c11be;
+  height: 100vh;
+  
+  h1{
+    text-align: center;
+    font-family: 'Saira Stencil One';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 32px;
+    line-height: 50px;
+    margin-bottom: 24px;
+    color: #FFFFFF;
+  }
+
+  input[type=email]{
+    width: 350px;
+    height: 58px;
+    background: #FFFFFF;
+    border-radius: 5px;
+    border: none;
+    outline: none;
+    margin-bottom: 13px;
+
+    font-family: 'Raleway';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 17px;
+    line-height: 23px;
+    color: #000000;
+    padding-left: 15px;
+  }
+
+  input[type=password]{
+    width: 100%;
+    max-width: 375px;
+    height: 58px;
+    background: #FFFFFF;
+    border-radius: 5px;
+    border: none;
+    outline: none;
+    margin-bottom: 13px;
+
+    font-family: 'Raleway';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 17px;
+    line-height: 23px;
+    color: #000000;
+    padding-left: 15px;
+  }
+
+ button{
+    width: 100%;
+    max-width: 375px;
+    height: 46px;
+    background: #A328D6;
+    border-radius: 5px;
+    border: none;
+    cursor: pointer;
+    margin: 0 auto;
+    
+    font-family: 'Raleway';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 18px;
+    line-height: 23px;
+    color: #FFFFFF;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-bottom: 36px;
+  }
+
+  p {
+    font-family: 'Raleway';
+    font-style: normal;
+    font-weight: 700;
+    font-size: 15px;
+    line-height: 18px;
+    color: #FFFFFF;
+    text-align: center;
+    cursor: pointer;
+  }
+`;
+
+/*
+
+	event.preventDefault();
+
+        setLoading("true")
+
+		try{
+			const requisicao = axios.post("https://mock-api.bootcamp.respondeai.com.br/api/v2/trackit/auth/login", {
+    
+            email: email,
+            password: senha
+            
+		})
+
+            requisicao.then((req)=>{
+                localStorage.setItem("imguser", req.data.image)
+                localStorage.setItem("token", req.data.token)
+                navigate("/habitos")
+            })
+            .catch(()=>{
+                setLoading("false")
+            })
+
+		}catch {
+			console.log("deu ruim")
+            
+            setEmail("")
+            setSenha("")
+		}
+	}
+
+    return (
+        <Styledlogin loading={loading}>
+            <form onSubmit={fazerLogin}>
+                <img src={imglogin} alt="imgagem de login"/>
+                
+                <input required type="text" id="campoemail" placeholder="email" value={email} onChange={e => setEmail(e.target.value)} /><br/>
+                
+                <input required type="password" id="camposenha" placeholder="senha" value={senha} onChange={e => setSenha(e.target.value)} /><br/>
+                <button>{loading === "true" ? (<ThreeDots color="#FFFFFF" height={80} width={80} />) : "Entrar"}</button>
+                <p onClick={()=>{navigate("/cadastro")}}>Não tem uma conta? Cadastre-se!</p>
+            </form>
+        </Styledlogin>
+    )
+}
+
+*/
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "../src/components/App"
+import "../src/css/reset.css"
+import "../src/css/style.css"
+
+ReactDOM.render(<App/>, document.querySelector('.root'))
